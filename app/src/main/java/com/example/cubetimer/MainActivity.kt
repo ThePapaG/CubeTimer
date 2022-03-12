9025ff635ec6662b166ab2dc2e969269bd6f479c
Papa-G <gdare1@yahoo.com.au>
2022-03-12 22:16:06 +1000

    Stopwatch in middle of screen

--- app/src/main/java/com/example/cubetimer/MainActivity.kt
+++ app/src/main/java/com/example/cubetimer/MainActivity.kt
@@ -1,11 +1,116 @@
 package com.example.cubetimer
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
 import android.os.Bundle
+import android.os.Handler
+import android.view.Gravity
+import android.view.View
+import android.widget.TextView
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : Activity() {
+    private var time = 0
+
+    // Is the stopwatch running?
+    private var running = false
+    private var wasRunning = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (savedInstanceState != null) {
+
+            // Get the previous state of the stopwatch
+            // if the activity has been
+            // destroyed and recreated.
+            time = savedInstanceState
+                .getInt("seconds")
+            running = savedInstanceState
+                .getBoolean("running")
+            wasRunning = savedInstanceState
+                .getBoolean("wasRunning")
+        }
+        runTimer()
+    }
+
+    // Save the state of the stopwatch
+    // if it's about to be destroyed.
+    public override fun onSaveInstanceState(
+        savedInstanceState: Bundle
+    ) {
+        savedInstanceState
+            .putInt("seconds", time)
+        savedInstanceState
+            .putBoolean("running", running)
+        savedInstanceState
+            .putBoolean("wasRunning", wasRunning)
+    }
+
+    // If the activity is paused,
+    // stop the stopwatch.
+    override fun onPause() {
+        super.onPause()
+        wasRunning = running
+        running = false
+    }
+
+    // If the activity is resumed,
+    // start the stopwatch
+    // again if it was running previously.
+    override fun onResume() {
+        super.onResume()
+        if (wasRunning) {
+            running = true
+        }
+    }
+
+    // Sets the NUmber of seconds on the timer.
+    // The runTimer() method uses a Handler
+    // to increment the seconds and
+    // update the text view.
+    private fun runTimer() {
+
+        // Get the text view.
+        val timeView = findViewById<View>(
+            R.id.time_view
+        ) as TextView
+
+        // Creates a new Handler
+        val handler = Handler()
+
+        // Call the post() method,
+        // passing in a new Runnable.
+        // The post() method processes
+        // code without a delay,
+        // so the code in the Runnable
+        // will run almost immediately.
+        handler.post(object : Runnable {
+            override fun run() {
+                val hours = time / 3600
+                val minutes = time % 3600 / 60
+                val secs = time % 60
+
+                // Format the seconds into hours, minutes,
+                // and seconds.
+                val timeString = String.format(
+                    Locale.getDefault(),
+                    "%d:%02d:%02d", hours,
+                    minutes, secs
+                )
+
+                // Set the text view text.
+                timeView.text = timeString
+
+                // If running is true, increment the
+                // seconds variable.
+                if (running) {
+                    time++
+                }
+
+                // Post the code again
+                // with a delay of 1 second.
+                handler.postDelayed(this, 1)
+            }
+        })
     }
 }
